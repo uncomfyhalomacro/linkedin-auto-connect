bcfbb16bdb68df4b2580b4ecb2305251e9ac78d9
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-29 01:14:33 +0800

    fix: update schemas to have the nonce column

--- migrations/up/0001-scraper-profile.sql
+++ migrations/up/0001-scraper-profile.sql
@@ -3,5 +3,6 @@ CREATE TABLE scraper_profiles (
     secret     TEXT    NOT NULL,
     first_used  TIMESTAMP NOT NULL,
     last_used   TIMESTAMP NOT NULL,
-    connections INT DEFAULT 0
+    connections INT DEFAULT 0,
+    nonce       INTEGER     DEFAULT 0
 );
--- migrations/up/0002-profile-links.sql
+++ migrations/up/0002-profile-links.sql
@@ -2,7 +2,8 @@ CREATE TABLE profile_links (
     id UUID         PRIMARY     KEY,
     fetched_at      TIMESTAMP   NOT NULL,
     updated_at      TIMESTAMP   NOT NULL,
-    memberIdUrl     TEXT        NOT NULL,
-    cleanProfileUrl TEXT        NOT NULL,  
-    name            TEXT        NOT NULL         
+    "memberIdUrl"     TEXT        NOT NULL,
+    "cleanProfileUrl" TEXT        NOT NULL,  
+    name            TEXT        NOT NULL,
+    nonce           INTEGER     DEFAULT 0        
 );
