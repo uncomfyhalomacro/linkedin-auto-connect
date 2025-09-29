f9ab8ed351360d1a3aa8b98f7fe4a7ed501b661d
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-30 06:58:20 +0800

    format: run formatter on sql

--- migrations/up/0001-scraper-profile.sql
+++ migrations/up/0001-scraper-profile.sql
@@ -1,8 +1,9 @@
-CREATE TABLE scraper_profiles (
-    id      UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
-    secret     TEXT    NOT NULL,
-    first_used  TIMESTAMP NOT NULL,
-    last_used   TIMESTAMP NOT NULL,
-    connections INT DEFAULT 0,
-    nonce       INTEGER     DEFAULT 0
-);
+CREATE TABLE
+    scraper_profiles (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        secret TEXT NOT NULL,
+        first_used TIMESTAMP NOT NULL,
+        last_used TIMESTAMP NOT NULL,
+        connections INT DEFAULT 0,
+        nonce INTEGER DEFAULT 0
+    );
