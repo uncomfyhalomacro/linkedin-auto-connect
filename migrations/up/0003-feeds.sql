dc95f4d5fcb59bae2b7f2c6f6b397405c907f4ff
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-30 07:07:01 +0800

    refactor: add a nonce column

--- migrations/up/0003-feeds.sql
+++ migrations/up/0003-feeds.sql
@@ -2,5 +2,6 @@ CREATE TABLE
     feeds (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         fetched_at TIMESTAMP NOT NULL,
-        interacted_on TIMESTAMP NOT NULL
+        interacted_on TIMESTAMP NOT NULL,
+        nonce INTEGER DEFAULT 0
     );
