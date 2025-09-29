4a751b47cd5fc204aebd3deebef74fb5538b41fb
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-30 07:01:30 +0800

    chore: add the missing columns

--- migrations/up/0002-profile-links.sql
+++ migrations/up/0002-profile-links.sql
@@ -1,9 +1,10 @@
-CREATE TABLE profile_links (
-    id UUID         PRIMARY     KEY,
-    fetched_at      TIMESTAMP   NOT NULL,
-    updated_at      TIMESTAMP   NOT NULL,
-    "memberIdUrl"     TEXT        NOT NULL,
-    "cleanProfileUrl" TEXT        NOT NULL,  
-    name            TEXT        NOT NULL,
-    nonce           INTEGER     DEFAULT 0        
-);
+CREATE TABLE
+    profile_links (
+        id UUID PRIMARY KEY,
+        fetched_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
+        member_id_url TEXT NOT NULL,
+        clean_url_profile TEXT NOT NULL,
+        name TEXT NOT NULL,
+        nonce INTEGER DEFAULT 0
+    );
