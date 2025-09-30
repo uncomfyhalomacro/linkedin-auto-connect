91988d1a2e323ad3c2e0800650f9fad81171ac2b
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-30 20:45:16 +0800

    fix: sql column should be named fetched_on

--- docs/ARCHITECTURE.md
+++ docs/ARCHITECTURE.md
@@ -109,7 +109,7 @@ CREATE TABLE
     feeds (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         post_url TEXT,
-        fetched_at TIMESTAMP NOT NULL,
+        fetched_on TIMESTAMP NOT NULL,
         interacted_on TIMESTAMP NOT NULL,
         nonce INTEGER DEFAULT 0,
         UNIQUE(post_url)
--- migrations/up/0003-feeds.sql
+++ migrations/up/0003-feeds.sql
@@ -2,7 +2,7 @@ CREATE TABLE
     feeds (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         post_url TEXT NOT NULL,
-        fetched_at TIMESTAMP NOT NULL,
+        fetched_on TIMESTAMP NOT NULL,
         interacted_on TIMESTAMP NOT NULL,
         nonce INTEGER DEFAULT 0,
         UNIQUE(post_url)
