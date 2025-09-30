de36ceb406df679cfe9d3dca5a513e366197f7a3
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-09-30 20:37:29 +0800

    chore: sql  column post_url should not be null

--- migrations/up/0003-feeds.sql
+++ migrations/up/0003-feeds.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
     feeds (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        post_url TEXT,
+        post_url TEXT NOT NULL,
         fetched_at TIMESTAMP NOT NULL,
         interacted_on TIMESTAMP NOT NULL,
         nonce INTEGER DEFAULT 0,
